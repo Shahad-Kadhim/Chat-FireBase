78eb81663f88db790e701dea1728ec9509fcb1e9
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-12-20 18:00:16 +0300

    change to fire store

--- app/src/main/java/com/shahad/app/chat/data/ChatFireBase.kt
+++ app/src/main/java/com/shahad/app/chat/data/ChatFireBase.kt
@@ -1,36 +1,34 @@
 package com.shahad.app.chat.data
 
 import android.util.Log
-import com.google.firebase.database.*
-import com.google.firebase.database.ktx.*
+import com.google.firebase.firestore.ktx.*
 import com.google.firebase.ktx.Firebase
 import javax.inject.Inject
 
 class ChatFireBase @Inject constructor(){
-    private val db = Firebase.database
-    private val messageReference = db.getReference("messages")
+    private val db = Firebase.firestore
+    private val messageReference = db.collection("messages")
 
     fun add(message: Message){
-        messageReference.push().setValue(message).addOnSuccessListener {
+        messageReference.add(message).addOnSuccessListener {
             Log.i("CHAT_LOG","Success Add")
         }.addOnFailureListener {
             Log.i("CHAT_LOG","fail Add")
-
         }
     }
 
     fun getNewMessage(onGetMessages: (List<Message>) -> Unit){
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                dataSnapshot.children.map { it.getValue<Message>() }.let {
-                    onGetMessages(it.filterNotNull())
-                }
-            }
+        messageReference.addSnapshotListener { value, _ ->
 
-            override fun onCancelled(databaseError: DatabaseError) {
+            value?.let { snapShot ->
+                snapShot
+                    .toObjects<Message>()
+                    .sortedBy { it.date }
+                    .also {
+                        onGetMessages(it)
+                    }
             }
         }
-        messageReference.addValueEventListener(postListener)
     }
 
 }
